14f727f85b26dd44da759613351517f971988a1f
AlexLojko <a_l_g@tut.by>
2023-02-24 00:15:15 +0300

    add two Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Input a number1: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input a number2: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 > number2)
+    Console.WriteLine("max = " + number1);
+else
+    Console.WriteLine("max = " + number2);
+    */
+
+//Task2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("Input a number1: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input a number2: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input a number3: ");
+double number3 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 > number2 && number1 > number3)
+    Console.WriteLine("max = " + number1);
+else if (number2 > number1 && number2 > number3)
+    Console.WriteLine("max = " + number2);
+else if (number3 > number1 && number3 > number2)
+    Console.WriteLine("max = " + number3);
+else
+    Console.WriteLine("max = multiple numbers");
+    */
+
+/*Task3: Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).*/
